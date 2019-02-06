506df8e4465bb0b6d626d9e62eceaa244d78c10a
Luiz Pereira <luizfper@gmail.com>
2019-02-06 17:22:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,12 +3,12 @@ require 'pry'
 
 def load_library (file)
   fin_hashao={}
-  fin_hashao[:get_meaning]={}
-  fin_hashao[:get_emoticon]={}
+  fin_hashao["get_meaning"]={}
+  fin_hashao["get_emoticon"]={}
   hashao=YAML.load_file(file)
   hashao.each do |meaning,emoticons|
-    fin_hashao[:get_meaning][emoticons[1]]=meaning
-    fin_hashao[:get_emoticon][emoticons[0]]=emoticons[1]
+    fin_hashao["get_meaning"][emoticons[1]]=meaning
+    fin_hashao["get_emoticon"][emoticons[0]]=emoticons[1]
   end
   fin_hashao
 end
