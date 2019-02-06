b27af5519d8e2c70d5db17a1bbcd11314ad78e6d
Luiz Pereira <luizfper@gmail.com>
2019-02-06 17:23:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,8 +13,8 @@ def load_library (file)
   fin_hashao
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file,emoticon)
+  dictionary=load_library(file)
 end
 
 def get_english_meaning
